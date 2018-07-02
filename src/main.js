4bf98d8b01661ad835e3ad0772848186cd46f901
dengpeng <dengpeng@fangjinsuo.com>
2018-07-02 13:22:47 +0800

    fetchAPi

--- src/main.js
+++ src/main.js
@@ -11,12 +11,40 @@ export default {
   // 这个字段走 app.json
   config: {
     // 页面前带有 ^ 符号的，会被编译成首页，其他页面可以选填，我们会自动把 webpack entry 里面的入口页面加进去
-    pages: ['pages/logs/main', '^pages/index/main'],
+    pages: [
+      'pages/logs/main',
+      '^pages/index/main'
+    ],
     window: {
       backgroundTextStyle: 'light',
       navigationBarBackgroundColor: '#fff',
-      navigationBarTitleText: 'WeChat',
+      navigationBarTitleText: 'V2EX',
       navigationBarTextStyle: 'black'
-    }
+    },
+    // tabBar: {
+    //   color: "#92969c",
+    //   selectedColor: "#03a5ef",
+    //   borderStyle: "white",
+    //   backgroundColor: "#f7f7fa",
+    //   list: [{
+    //       pagePath: "pages/index/index",
+    //       iconPath: "image/home-icon.png",
+    //       selectedIconPath: "image/home-icon-active.png",
+    //       text: "首页"
+    //     },
+    //     {
+    //       pagePath: "pages/elite/elite",
+    //       iconPath: "image/elite-icon.png",
+    //       selectedIconPath: "image/elite-icon-active.png",
+    //       text: "找精英"
+    //     },
+    //     {
+    //       pagePath: "pages/release/release",
+    //       iconPath: "image/release-icon.png",
+    //       selectedIconPath: "image/release-icon.png",
+    //       text: "发布"
+    //     }
+    //   ]
+    // }
   }
 }
